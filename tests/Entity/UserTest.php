8ea078deca10b49fedd6107921123686b5b26872
AdrienLutz <adrlutz@gmail.com>
2024-04-10 16:57:16 +0200

    src > test/TestUser.php : bug 2 erreurs

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -1,41 +1,46 @@
 <?php
-//
-//namespace App\Tests\Entity;
-//
-//use App\Entity\User;
-//use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-//
-//class UserTest extends TestCase
-//{
-//    public function getEntity(): User
-//    {
-//        return (new User())
-//            ->setEmail('phptest@mail.fr')
-//            ->setPassword('azerty')
-//            ->setRoles(['ROLE_USER'])
-//            ->setVerified(true)
-//            ->setfirstName('phptest')
-//            ->setlastName('phptest');
-//        }
-//    }
-//
-//    public function getErrors(User $user, int $number = 0)
-//    {
-//        self::bootKernel();
-//        //pour acceder au noyau symfonycasts
-//        $container = static::getContainer();
-//        $errors = $container->get('validator')->validate($user);
-//        $this->assertCount($number, $errors);
-//    }
-//
-//    public function testValidUserEntity()
-//    {
-//        $this->getErrors($this->getEntity(), 0);
-//    }
-//
-//    public function testInvalidEmail()
-//    {
-//        $this->getErrors($this->getEntity()->setEmail(444), 2); // Correction de la syntaxe de l'appel à la méthode setEmail
-//    }
-//
-//}
+
+namespace App\Tests\Entity;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class UserTest extends KernelTestCase
+{
+
+   //on cree un user
+    public function getEntity(): User
+    {
+        return (new User())
+            ->setEmail('phptest@mail.fr')
+            ->setPassword('azerty')
+            ->setRoles(['ROLE_USER'])
+            ->setVerified(true)
+            ->setfirstName('phptest')
+            ->setlastName('phptest');
+    }
+
+//    on recupere les erreurs
+    public function getErrors(User $user, int $number = 0)
+    {
+        self::bootKernel();
+        //pour acceder au noyau symfonycasts
+        $container = static::getContainer();
+        $errors = $container->get('validator')->validate($user);
+        $this->assertCount($number, $errors);
+    }
+
+//    on compte les erreurs
+    public function testValidUserEntity()
+    {
+        $this->getErrors($this->getEntity(), 2);
+//        $this->getErrors($this->getEntity(), 1);
+    }
+// on crée les erreurs
+    public function testInvalidEmail()
+    {
+        $this->getErrors($this->getEntity()->setEmail('')->setPassword(15), 2);
+//        $this->getErrors($this->getEntity()->setEmail('testphp@mail.fr')->setPassword('azerty'), 0);
+    }
+
+}
